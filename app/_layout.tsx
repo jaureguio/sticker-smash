905f50c93a59b72784aa9d886e5b8df3f9d6db90
Oscar Jauregui <oscarjaureguia@gmail.com>
2024-10-19 16:20:33 -0400

    style: change splash screen background and status bar color

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,17 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { useEffect } from 'react';
+import { setStatusBarStyle } from 'expo-status-bar';
 
 export default function RootLayout() {
+    useEffect(() => {
+        setTimeout(() => {
+            setStatusBarStyle('light');
+        }, 0);
+    });
+
     return (
         <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="+not-found" />
         </Stack>
     );
 }
